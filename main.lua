c400c5d1618f03399ebca9725318f9332a4a97ed
Brad Herman <brad.herman@gmail.com>
2012-01-28 13:48:52 -0800

    faster, better

--- main.lua
+++ main.lua
@@ -8,13 +8,32 @@
 ---- cpmgen main.lua
 
 local terra = require("terra")
-
+terra.debug = true
 local physics = require("physics")
+--physics.setDrawMode( "hybrid" )
 physics.start()
+physics.pause()
+physics.setGravity( 0, 9.8 )
 
 display.setStatusBar(display.HiddenStatusBar)
 
---test = display.newRect(0,0,128,64)
---test:setFillColor ( 64, 100, 27 ,255 )
 
-terra.newTerrain(10,10,15,15,"rect")
+local ball = display.newRoundedRect(  50, 0, 25, 25, 15 )
+physics.addBody( ball, { density = 1.0, friction = 0.3, bounce = 0.2, radius = 13 } )
+
+
+local ball = display.newRoundedRect(  100, 0, 25, 25, 15 )
+physics.addBody( ball, { density = 1.0, friction = 0.3, bounce = 0.2, radius = 13 } )
+
+
+local ball = display.newRoundedRect(  150, 0, 25, 25, 15 )
+physics.addBody( ball, { density = 1.0, friction = 0.3, bounce = 0.2, radius = 13 } )
+
+
+terra.newTerrain(0,100,32,32,10,10,"rect")
+
+
+physics.start( true )
+
+
+Runtime:addEventListener ("enterFrame",terra.enterFrameProcess)		--this event processes our terrain each frame
--- terra.lua
+++ terra.lua
@@ -9,26 +9,28 @@ M.h = 10
 M.terraGrid = {}
 M.terraLookup = {}
 M.displayGroup = display.newGroup ( )
+M.removeQueue = {}
+M.debug = false
 
 
 -- functions are now local:
-local newTerrain = function(width,height,pxW,pxH,img)
+local newTerrain = function(x,y,width,height,pxW,pxH,img)
     print( "init New Terrain" )
     M.w = width
     M.h = height
-    
+    local count = 1
     for w = 1, M.w do
     	M.terraGrid[w]= {}
-    	local count = 1
     	for h = 1, M.h do
     		if img == "rect" then
-    			M.terraGrid[w][h] = display.newRect( M.displayGroup, (w-1)*pxW, (h-1)*pxH, pxW, pxH )  --Create a Rect for Terrain, this should be an image eventualy
+    			M.terraGrid[w][h] = display.newRect( M.displayGroup, (w-1)*pxW+x, (h-1)*pxH+y, pxW, pxH )  --Create a Rect for Terrain, this should be an image eventualy
+    			--display.newText(  count,  (w-1)*pxW+x, (h-1)*pxH+y,nil,10)
     			M.terraGrid[w][h]:addEventListener ( "touch", M.terrainTouch )
     			M.terraGrid[w][h]:setFillColor ( math.random(0,255), math.random(0,255), math.random(0,255)  )
     			M.terraGrid[w][h].name = count
     			M.terraLookup[count] = {}
     			M.gridLookup(count)
-    			physics.addBody ( M.terraGrid[w][h],  "static",{density=.5, friction=.03, bounce=.5} )
+    			physics.addBody ( M.terraGrid[w][h],  "static",{density=.5, friction=.1, bounce=.4} )
     		end
     		count = count + 1
     	end
@@ -39,10 +41,37 @@ end
 M.newTerrain = newTerrain
 
 local terrainTouch = function(event)
-    print( "Touched it",event.phase )
+    if M.debug then print( "Touched it",event.phase ,event.target.name) end
+    --M.removeBlock(event.target)
+    M.removeQueue[event.target] = event.target
     --remove the object touched here
 end
 M.terrainTouch = terrainTouch
+
+
+local removeBlock = function(target)
+	if target then
+	if M.debug then print("removing",target.name) end
+	physics.removeBody(target ) 	 	--remove our physics objects
+	display.remove(target)					-- remove display objects
+	--M.displayGroup:remove(target)
+	target = nil 									-- remove the display object from memory now
+	end
+end
+M.removeBlock = removeBlock
+
+
+
+local enterFrameProcess = function()
+	--print(#M.removeQueue)
+	for k,v in pairs(M.removeQueue) do					 
+			M.removeBlock(M.removeQueue[k])			
+	end
+	M.removeQueue = {}
+
+
+end
+M.enterFrameProcess = enterFrameProcess
 
 local gridTest = function(target)
 	-- for loop of the array in side here: M.terraLookup[target.name]
